f153fc195214b6ca9d177db122ab01c4f9ae526c
Sümeyye Yerli <33040732+Sumeye@users.noreply.github.com>
2022-03-03 00:56:14 +0300

    infrastructure

---
+++ NLayer.Repository/Configuration/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Configuration
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired().IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("Decimal(18,2)");
+            builder.ToTable("Products");
+
+            builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
+
+        }
+    }
+}
