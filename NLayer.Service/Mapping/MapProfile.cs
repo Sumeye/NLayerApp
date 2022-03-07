4a721a9f9bc03a80267cd9bc61a89cc487dab5c4
Sümeyye Yerli <33040732+Sumeye@users.noreply.github.com>
2022-03-07 23:12:50 +0300

    API Katmanındaki CRUD methodları yazıldı.

---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeature>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+     
+    }
+}
